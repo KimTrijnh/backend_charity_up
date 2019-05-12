03d56ba1591c61f07d860e54dac2ee579ac1694a
Huong Trinh <trtrhuong@gmail.com>
2019-05-13 02:25:32 +0700

    draf data schema

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,13 @@
 
-from flask import Flask, redirect,render_template, url_for, flash, jsonify, request
+from src.models import *
+from flask import Flask, redirect, render_template, url_for, flash, jsonify, request
 from flask_cors import CORS
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import login_required, current_user, LoginManager
 
-app=Flask(__name__)
+app = Flask(__name__)
 CORS(app)
 
 app.config.from_object(Config)
@@ -14,7 +15,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager()
 login_manager.init_app(app)
-from src.models import *
+
 
 @app.route('/')
 @app.route('/home')
@@ -22,6 +23,8 @@ def home():
     return 'hello'
 
 # AUTH VIEWS
+
+
 def check_username(username):
     user = UserVisit.query.filter_by(username=username).first()
     if user:
@@ -29,12 +32,14 @@ def check_username(username):
     else:
         return False
 
+
 def check_email(email):
     user = UserVisit.query.filter_by(email=email).first()
     if user:
         return True
     else:
-        return False  
+        return False
+
 
 @app.route('/signup', methods=['POST'])
 def signup():
@@ -48,8 +53,8 @@ def signup():
             return jsonify({
                 'success': True,
                 'user_id': user.id,
-                'username' : user.username
-                })
+                'username': user.username
+            })
         else:
             return jsonify({
                 'success': False,
@@ -58,22 +63,82 @@ def signup():
     return jsonify({'message': 'failed'})
 
 
-
 @app.route('/login', methods=['POST'])
 def login():
     if request.method == 'POST':
         data = request.get_json()
         user = UserVisit.query.filter_by(username=data['username']).first()
         if user and user.check_password(data['password']):
-            return jsonify({'isLogin': True, 'current_user': user.username })
+            return jsonify({'isLogin': True, 'current_user': user.username})
         else:
             return jsonify({'isLogin': False, 'message': 'Wrong Username/Password'})
     return redirect("http://localhost:3000/login")
-    
 
-# @app.route('/current_user', methods=['GET', 'POST'])
-# def current_user():
-#     if current_user:
+
+@app.route('/team/create', methods=['POST'])
+def create_team():
+    if request.method == 'POST':
+        data = request.get_json()
+        team = Team(name=data['name'], description=data['description'], location=data['location'], email=data['email'], user_id=data['user_id'],
+                    isActive=data['isActive'], img_url=data['img_url'])
+        if team:
+            db.sesson.add(team)
+            db.session.commit()
+            return jsonify({'success': True, 'team_id': team.id})
+        else:
+            return jsonify({'success': False, 'error': 'invalid input'})
+    return jsonify({'message': 'invalid method'})
+
+
+@app.route('/teams', methods=['GET'])
+def teams():
+    if request.method == 'GET':
+        teams = Team.query.all()
+        teamArray = []
+        for team in teams:
+            teamArray.append({
+                'id' : team.id,
+                'img_url': team.img_url,
+                'name' : team.name,
+                'description': team.description,
+                'user_id' : team.user_id,
+                'location': team.location,
+                'create_at': team.create_at,
+                'email': team.email,
+                'isActive': team.isActive,
+                'total_rating':
+                'rating':
+                'total_campaign':
+                'total_member':
+            })
+        return jsonify({'success': True, teamArray})
+    return jsonify({ 'success': False, 'message': 'invalid method'})
+
+
+@app.route('/team/<int:id>', methods=['GET'])
+def team(id):
+    if request.method == 'GET':
+        team = Team.query.filter_by(id=id).first()
+        if team:
+            teamDict = {
+                'id' : team.id,
+                'img_url': team.img_url,
+                'name' : team.name,
+                'description': team.description,
+                'user_id' : team.user_id,
+                'location': team.location,
+                'create_at': team.create_at,
+                'email': team.email,
+                'isActive': team.isActive,
+                'total_rating':
+                'rating':
+                'total_campaign':
+                'total_member':
+                }
+            return jsonify({'success': True, 'team' : teamDict})
+        else:
+            return jsonify({'success': False, 'message': 'team not found'})
+    return jsonify({'message': 'invalid method'})
 
 
 @login_manager.user_loader
--- src/models.py
+++ src/models.py
@@ -24,4 +24,99 @@ class Token(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey(UserVisit.id), nullable=False)
     user = db.relationship(UserVisit)
 
-    
+
+class Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False, unique=True)
+    description = db.Column(db.String, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    email= db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(UserVisit.id), nullable=False)
+    create_at = db.Column(db.DateTime, )
+    isActive = db.Column(db.Boolean, default=False)
+    img_url= db.Column(db.String, nullable=True)
+    # members = db.Column()
+    # campaigns = db.Column()
+    # reviews = db.Column()
+
+class Campaigns(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False, unique=True)
+    description = db.Column(db.String, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey(Team.id), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(UserVisit.id), nullable=True)
+    create_at = db.Column(db.DateTime)
+    start_at = db.Column(db.DateTime)
+    end_at = db.Column(db.DateTime)
+    img_url = db.Column(db.String)
+    bank_id = db.Column(db.Integer, db.ForeignKey(Bank.id), default=None)
+    # donations = db.Column()
+    #members
+    #reviews
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=True)
+    text = db.Column(db.String, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(UserVisit.id), nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey(Team.id))
+    campaign_id = db.Column(db.Integer, db.ForeignKey(Campaign.id))
+
+class Donation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(UserVisit.id), nullable=False)
+    campaign_id = db.Column(db.Integer, db.ForeignKey(Campaign.id))
+
+class Bank(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    provider = db.Column(db.String, nullable=False)
+    account_no = db.Column(db.Integer, nullable=False)
+    campaign_id = db.Column(db.Integer, db.ForeignKey(Campaign.id), nullable=False)
+
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String)
+    lat = db.Column(db.Float)
+    long = db.Column(db.Float)
+    team_id = db.Column(db.Integer, db.ForeignKey(Team.id))
+    campaign_id = db.Column(db.Integer, db.ForeignKey(Campaign.id))
+
+
+class SubCampaign(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    campaign_id = db.Column(db.Integer, db.ForeignKey(Campaign.id))
+    start_at = 
+    end_at =
+    location = 
+
+# class Image(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     team_id
+#     user_id
+#     campaign_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
