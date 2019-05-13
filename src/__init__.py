0238afd23c50a4c7ac0fad40c0e86b156e504794
Huong Trinh <trtrhuong@gmail.com>
2019-05-13 14:40:36 +0700

    comment out

--- src/__init__.py
+++ src/__init__.py
@@ -75,70 +75,70 @@ def login():
     return redirect("http://localhost:3000/login")
 
 
-@app.route('/team/create', methods=['POST'])
-def create_team():
-    if request.method == 'POST':
-        data = request.get_json()
-        team = Team(name=data['name'], description=data['description'], location=data['location'], email=data['email'], user_id=data['user_id'],
-                    isActive=data['isActive'], img_url=data['img_url'])
-        if team:
-            db.sesson.add(team)
-            db.session.commit()
-            return jsonify({'success': True, 'team_id': team.id})
-        else:
-            return jsonify({'success': False, 'error': 'invalid input'})
-    return jsonify({'message': 'invalid method'})
+# @app.route('/team/create', methods=['POST'])
+# def create_team():
+#     if request.method == 'POST':
+#         data = request.get_json()
+#         team = Team(name=data['name'], description=data['description'], location=data['location'], email=data['email'], user_id=data['user_id'],
+#                     isActive=data['isActive'], img_url=data['img_url'])
+#         if team:
+#             db.sesson.add(team)
+#             db.session.commit()
+#             return jsonify({'success': True, 'team_id': team.id})
+#         else:
+#             return jsonify({'success': False, 'error': 'invalid input'})
+#     return jsonify({'message': 'invalid method'})
 
 
-@app.route('/teams', methods=['GET'])
-def teams():
-    if request.method == 'GET':
-        teams = Team.query.all()
-        teamArray = []
-        for team in teams:
-            teamArray.append({
-                'id' : team.id,
-                'img_url': team.img_url,
-                'name' : team.name,
-                'description': team.description,
-                'user_id' : team.user_id,
-                'location': team.location,
-                'create_at': team.create_at,
-                'email': team.email,
-                'isActive': team.isActive,
-                'total_rating':
-                'rating':
-                'total_campaign':
-                'total_member':
-            })
-        return jsonify({'success': True, teamArray})
-    return jsonify({ 'success': False, 'message': 'invalid method'})
+# @app.route('/teams', methods=['GET'])
+# def teams():
+#     if request.method == 'GET':
+#         teams = Team.query.all()
+#         teamArray = []
+#         for team in teams:
+#             teamArray.append({
+#                 'id' : team.id,
+#                 'img_url': team.img_url,
+#                 'name' : team.name,
+#                 'description': team.description,
+#                 'user_id' : team.user_id,
+#                 'location': team.location,
+#                 'create_at': team.create_at,
+#                 'email': team.email,
+#                 'isActive': team.isActive,
+#                 'total_rating':
+#                 'rating':
+#                 'total_campaign':
+#                 'total_member':
+#             })
+#         return jsonify({'success': True, teamArray})
+#     return jsonify({ 'success': False, 'message': 'invalid method'})
 
 
-@app.route('/team/<int:id>', methods=['GET'])
-def team(id):
-    if request.method == 'GET':
-        team = Team.query.filter_by(id=id).first()
-        if team:
-            teamDict = {
-                'id' : team.id,
-                'img_url': team.img_url,
-                'name' : team.name,
-                'description': team.description,
-                'user_id' : team.user_id,
-                'location': team.location,
-                'create_at': team.create_at,
-                'email': team.email,
-                'isActive': team.isActive,
-                'total_rating':
-                'rating':
-                'total_campaign':
-                'total_member':
-                }
-            return jsonify({'success': True, 'team' : teamDict})
-        else:
-            return jsonify({'success': False, 'message': 'team not found'})
-    return jsonify({'message': 'invalid method'})
+# @app.route('/team/<int:id>', methods=['GET'])
+# def team(id):
+#     if request.method == 'GET':
+#         team = Team.query.filter_by(id=id).first()
+#         if team:
+#             teamDict = {
+#                 'id' : team.id,
+#                 'img_url': team.img_url,
+#                 'name' : team.name,
+#                 'description': team.description,
+#                 'user_id' : team.user_id,
+#                 'location': team.location,
+#                 'create_at': team.create_at,
+#                 'email': team.email,
+#                 'isActive': team.isActive,
+#                 'total_rating':
+#                 'rating':
+#                 'total_campaign':
+#                 'total_member':
+#                 }
+#             return jsonify({'success': True, 'team' : teamDict})
+#         else:
+#             return jsonify({'success': False, 'message': 'team not found'})
+#     return jsonify({'message': 'invalid method'})
 
 
 @login_manager.user_loader
